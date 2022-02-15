71c7de5c981a6fa1c418c99da5778791fe42ab86
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-14 22:51:03 -0700

    Update

---
+++ assets/js/script.min.js
@@ -0,0 +1,1 @@
+var jumboHeight=$(".jumbotron").outerHeight();function parallax(){var o=$(window).scrollTop();$(".jumbotron").css("background-position","center "+-.2*o+"px"),console.log(jumboHeight-o)}$(window).scroll((function(o){parallax()}));
